9fc234ec4c71b4b2b301dec46fa2a84e0dbadf7f
Vincent Dusza <duszavincent@gmail.com>
2023-03-29 13:38:31 +0200

    commit12

--- site/header.php
+++ site/header.php
@@ -11,12 +11,12 @@
 <body>
     <header>
         <nav id="mainNavbar">
-            <li><a class="padNavbarTxt"><a href="index.php">Home</a></li>
+            <li><a class="padNavbarTxt" href="index.php">Home</a></li>
             <li><a class="padNavbarTxt">Recepten</a></li>
             <li><a class="padNavbarTxt">Categorieen</a></li>
             <li><a class="borderZoeken"><input class="txtZoeken" type="text" placeholder="Zoek recepten.."></a></li>
             <li><a class="padNavbarTxt">Ingredienten</a></li>
-            <li><a class="padNavbarTxt"><a href="kookBoekSorteer.php">Tabel</a></li>
+            <li><a class="padNavbarTxt" href="kookBoekSorteer.php">Tabel</a></li>
             <li><a class="padNavbarTxt">Contact</a></li>
         </nav>
     </header>
