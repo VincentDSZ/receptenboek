151ff6bd0c285d413ee322b06cdd8c999c5ac1d6
Vincent Dusza <duszavincent@gmail.com>
2023-03-31 13:06:30 +0200

    commit19

--- site/footer.php
+++ site/footer.php
@@ -8,6 +8,38 @@
     <title>Document</title>
 </head>
 
+<style>
+    footer {
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: auto;
+        width: 100vw;
+        font-family: sans-serif;
+        padding-top: 40px;
+        color: #fff;
+    }
+
+    .footer-bottom {
+        background: #111;
+        width: 100vw;
+        padding: 20px 0px;
+        text-align: center;
+    }
+
+    .footer-bottom p {
+        font-size: 14px;
+        word-spacing: 2px;
+        text-transform: capitalize;
+    }
+
+    .footer-bottom span {
+        text-transform: uppercase;
+        opacity: .4;
+        font-weight: 200;
+    }
+</style>
+
 <body>
     <footer>
         <div class="footer-bottom">
